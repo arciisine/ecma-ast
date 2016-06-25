d1b4127d93b24d5ee74a7e40c42755d5218f3874
Timothy Soehnlin <tim@eaiti.com>
2016-06-25 14:57:22 -0500

    Rewriting visitor to allow for start/end events, allowing for finer control

---
+++ src/transform/index.ts
@@ -0,0 +1,42 @@
+/// <reference path="../../node_modules/@types/esprima/index.d.ts" />
+
+import * as _ from "lodash"
+import * as esprima from "esprima"
+import * as escodegen from '../escodegen';
+import {AST} from "../ast"
+import {Macro} from '../macro';
+import {Visitor, Handler} from './visitor';
+
+export class Transform {
+  
+  static parseExpression<T extends AST.Node>(expr:string):T {
+    let res =  (esprima.parse(expr) as any).body[0] as T;
+    return res;
+  }
+
+  static compileExpression(node:AST.Node):string {
+    return escodegen.generate(node);
+  }
+
+  static parse(fn:Function|string):AST.FunctionExpression {
+    return Transform.parseExpression<AST.FunctionExpression>(fn.toString());
+  }
+    
+  static compile(node:AST.FunctionExpression, globals:any):Function {
+    let genSym = Macro.genSymbol.toString();
+    genSym = 'function genSym ' + genSym.substring(genSym.indexOf('('))
+    let src = `(function() {     
+      var id_ = new Date().getTime();
+      var genSymbol = ${genSym};
+      ${Object.keys(globals || {}).map(k => `var ${k} = ${globals[k].toString()}`).join('\n')} 
+      return ${Transform.compileExpression(node)}; 
+    })()`;
+    return eval(src);
+  }
+
+  static rewrite(fn:Function, handlers:{[key:string]:Handler}, globals:any = {}) {
+    let ast = Transform.parse(fn); 
+    ast = new Visitor(handlers).exec(ast);
+    return Transform.compile(ast, globals);
+  }
+}
