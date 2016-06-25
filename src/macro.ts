0388eef76da88f586624cc3040251b4ba8209052
Timothy Soehnlin <tim@eaiti.com>
2016-06-25 08:47:30 -0500

    Restructuring transform api into a consistent lib

---
+++ src/macro.ts
@@ -0,0 +1,145 @@
+import {AST} from "./ast"
+
+let id_:number = new Date().getTime()%10000;
+
+export class Macro {
+
+  static genSymbol() {
+    return "__gen"+parseInt(`${Math.random()*1000}`)+(id_++); 
+  }
+
+  static Id = (name?:string):AST.Identifier => ({type:"Identifier", name:name||Macro.genSymbol()})
+  static Literal = (value:any):AST.Literal =>  ({type:"Literal",    value })
+	static Block = (...body):AST.BlockStatement => ({type:"BlockStatement", body:body.filter(x => !!x) })
+	static Expr = (n:AST.Node):AST.ExpressionStatement => ({type:"ExpressionStatement", expression:n})
+	static Continue = (label?:AST.Identifier):AST.ContinueStatement => ({type:"ContinueStatement", label})
+	static Noop = ():AST.Node => Macro.Block([])
+
+	static Return = (e:AST.Expression):AST.ReturnStatement => ({type:"ReturnStatement", argument:e});
+	static Yield = (e:AST.Expression, delegate:boolean = false):AST.YieldExpression => {
+    return {type:"YieldExpression", argument:e, delegate} as AST.YieldExpression
+  };
+
+	static Array = (size:number = 0):AST.ArrayPattern => {
+    return {
+      type: "ArrayPattern",
+      elements: []
+    };
+  } 
+
+	static Throw = (e:AST.Expression):AST.ThrowStatement => {return {type:"ThrowStatement", argument:e}};
+  static Call = (src:AST.Identifier|AST.Expression, ...args):AST.CallExpression => {
+    return {type:"CallExpression", callee:src, arguments:args.filter(x => !!x)}
+  };
+
+	static Assign = (id:AST.Identifier, expr:AST.Expression, op:string = '='):AST.AssignmentExpression => {
+    return {
+      type : "AssignmentExpression",
+      left : id,
+      operator : op as any as AST.AssignmentOperator,
+      right : expr
+    };
+  }
+
+	static GetProperty = (id:AST.Identifier, prop:AST.Identifier|string):AST.MemberExpression => {
+    return {
+      type : "MemberExpression",
+      computed : typeof prop !== 'string',
+      object : id,
+      property : typeof prop === 'string' ? Macro.Id(prop) : prop,
+    };
+  }
+  
+	static Vars = (...args):AST.VariableDeclaration => {
+    let kind:('var'|'const'|'let') = 'var';
+    if (args[0] === 'var' || args[0] === 'let' || args[0] === 'const') {
+      kind = args.shift();
+    }  
+    let decls = [];
+    for (let i = 0; i < args.length; i+=2) {
+      if (args[i] && i <=  args.length) {
+        decls.push({type:"VariableDeclarator", id:args[i], init:args[i+1]});
+      }
+    }
+    return {type:"VariableDeclaration", kind, declarations: decls};
+  }
+
+	static BinaryExpr = (id:AST.Identifier, op:string, val:AST.Expression):AST.BinaryExpression => {
+    return {
+      type : "BinaryExpression",
+      left : id,
+      operator : op as any as AST.BinaryOperator,
+      right : val
+    }
+  }
+
+  static UnaryExpr = (op:string, val:AST.Expression):AST.UnaryExpression => {
+    return {
+      type : "UnaryExpression",    
+      operator : op as any as AST.UnaryOperator,
+      prefix : true,
+      argument : val
+    }
+  }
+
+  static Negate = (val:AST.Expression):AST.UnaryExpression => {
+    return Macro.UnaryExpr("!", val);
+  }
+
+  static Increment = (id:AST.Identifier, increment:number = 1):AST.AssignmentExpression => {
+    return Macro.Assign(id, Macro.Literal(increment), '+=');
+  }
+
+  static Labeled = (id:AST.Identifier, body:AST.Statement):AST.LabeledStatement => {
+    return {
+      type : "LabeledStatement",
+      label : id,
+      body
+    };
+  }
+
+  static ForLoop = (id:AST.Identifier, init:AST.Expression, upto:AST.Expression, body:AST.Statement[], increment:number = 1):AST.ForStatement => {
+    return {
+      type : "ForStatement",
+      init: Macro.Vars(id, init),
+      test: Macro.BinaryExpr(id, '<', upto),
+      update: Macro.Increment(id, increment),
+      body: Macro.Block(...body)
+    };
+  }
+
+  static TryCatchFinally = (t:AST.Node[], c:AST.Node[] = [], f:AST.Node[] = []):AST.TryStatement => {
+    return {
+      type : "TryStatement",
+      block :  Macro.Block(...t),
+      handler : {
+        type: "CatchClause",
+        param : Macro.Id('e'),
+        body : Macro.Block(...c),
+      },
+      finalizer : Macro.Block(...f)
+    };
+  }
+  static Func = (id:AST.Identifier, params:AST.Pattern[], body:AST.Node[], generator:boolean = false):AST.FunctionDeclaration => {
+    return {
+      type : "FunctionDeclaration", 
+      id,
+      params, 
+      body : Macro.Block(...body), 
+      generator, 
+      defaults:[], 
+      expression:false
+    };
+  } 
+  static IfThen = (test:AST.Expression, body:AST.Node[], elseBody:AST.Node[] = []):AST.IfStatement => {
+    let res:any = {
+      type : "IfStatement",
+      test,
+      consequent : Macro.Block(...body)
+    }
+    if (elseBody) {
+      res['alternate']  = Macro.Block(...elseBody)
+    };
+    return res as AST.IfStatement;
+  } 
+}
---
+++ src/transform.ts
@@ -0,0 +1,83 @@
+/// <reference path="../node_modules/@types/lodash/index.d.ts" />
+/// <reference path="../node_modules/@types/esprima/index.d.ts" />
+
+import * as _ from "lodash"
+import * as esprima from "esprima"
+import * as escodegen from './escodegen';
+import {AST} from "./ast"
+import {Macro} from './macro';
+
+interface Transformer {
+  <T extends AST.Node>(node:T):T
+}
+
+interface Visitor { 
+  process:Transformer
+}
+
+export class Transform {
+  
+  static NESTED_PROPERTIES =  [
+    'body', 'declarations', 'argument', 'arguments', 'alternate', 'consequent',
+    'left', 'right', 'init', 'expression', 'callee', 'elements', 
+    'handlers', 'handler', 'block', 'finalizer', 'test', 'object', 'property'
+  ]
+
+  static visit<T extends AST.Node>(visitor:Visitor, node:T, parent:AST.Node|[AST.Node] = null, key:string|number = null):T {   
+    node = visitor.process(node);   
+    Transform.NESTED_PROPERTIES.filter(p => !!node[p])
+      .forEach(p => { 
+        let x = node[p];
+        if (Array.isArray(x)) {
+          x.forEach((y, i) => { Transform.visit(visitor, y, x, i); })
+        } else if (typeof x !== 'string' && typeof x !== 'boolean' && typeof x !== 'number') {
+          Transform.visit(visitor, x, node, p);
+        }
+      });
+
+    if (parent) parent[key] = node;
+    return node;
+  }
+  
+  static parse(fn:Function|string):AST.FunctionExpression {
+    let ast = <AST.FunctionExpression>(esprima.parse(fn.toString()) as any as AST.BlockStatement).body[0];
+    return ast;
+  }
+  
+  static compile(node:AST.FunctionExpression, globals:any):Function {
+    let genSym = Macro.genSymbol.toString();
+    let src = `(function() {     
+      var id_ = new Date().getTime();
+      var genSymbol = ${genSym};
+      ${Object.keys(globals || {}).map(k => `var ${k} = ${globals[k].toString()}`).join('\n')} 
+      return ${escodegen.generate(node)}; 
+    })()`;
+    console.log(src);
+    return eval(src);
+  }
+
+  static visitor(conf:{[key:string]:Transformer}) {
+    let out = {
+      process : function<T extends AST.Node>(node:T):AST.Node {
+        if (node['visited']) {
+          return node;
+        } else {
+          node['visited'] = true;
+          if (this[node['type'] as string]) {
+            let res = this[node['type'] as string](node);
+            return res || node; 
+          } else {
+            return node;
+          }
+        }
+      }
+    };
+    return _.extend(out, conf) as typeof out;
+  };
+  
+  static rewrite(fn:Function, visitor:Visitor, globals:any = {}) {
+    let ast = Transform.parse(fn); 
+    ast = Transform.visit(visitor, ast);
+    return Transform.compile(ast, globals);
+  }
+}
