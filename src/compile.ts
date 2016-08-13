56cf13d84a04785138895d0017a97e00587d4b9a
Timothy Soehnlin <tim@eaiti.com>
2016-08-13 09:50:43 -0500

    Fixing compile

--- src/compile.ts
+++ src/compile.ts
@@ -7,11 +7,12 @@ export class CompileUtil {
     return escodegen.generate(node);
   }
 
-  static compile(node:AST.BaseFunction, globals:any):Function {
+  static compile(node:AST.BaseFunction|string, globals:any, optimize:any = null):Function {
     let src = `(function() {     
       ${Object.keys(globals || {}).map(k => `var ${k} = ${globals[k].toString()}`).join('\n')} 
-      return ${CompileUtil.compileExpression(node)}; 
+      return ${typeof node === 'string' ? node : CompileUtil.compileExpression(node)}; 
     })()`;
+    
     return eval(src);
   }
 }
