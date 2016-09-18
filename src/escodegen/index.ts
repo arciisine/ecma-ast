6e5995222fab12e5f4b739460f4d8a7e3c3bdb01
Timothy Soehnlin <tim@eaiti.com>
2016-09-18 10:28:58 -0500

    Supporting config options

--- src/escodegen/index.ts
+++ src/escodegen/index.ts
@@ -1,3 +1,46 @@
 import {AST} from '../ast';
 import escodegen from './lib';
-export let generate:(node:AST.Node)=>string = escodegen['generate']
+
+export interface EscodegenOptionsFormatIndent {
+  style?: string;
+  base?: number;
+  adjustMultilineComment?: boolean;
+};
+
+export interface EscodegenOptionsMozilla {
+  comprehensionExpressionStartsWithAssignment: boolean;
+  starlessGenerator: boolean;
+}
+
+export interface EscodegenOptionsFormat {
+  indent: EscodegenOptionsFormatIndent
+  newline?: string;
+  space?: string;
+  json?: boolean;
+  renumber?: boolean;
+  hexadecimal?: boolean;
+  quotes?: 'single'|'double';
+  escapeless?: boolean;
+  compact?: boolean;
+  parentheses?: boolean;
+  semicolons?: boolean;
+  safeConcatenation?: boolean;
+  preserveBlankLines?: boolean;
+}
+export interface EscodegenOptions {
+  indent?: boolean;
+  base?: string;
+  parse?: (node:AST.Node)=>string;
+  comment?: boolean;
+  format?:EscodegenOptionsFormat;
+  moz?: EscodegenOptionsMozilla;
+  directive?: boolean;
+  raw?: boolean;
+  verbatim?: string;
+  sourceMap?: any;
+  sourceMapRoot: any;
+  sourceMapWithCode: any;
+  sourceCode?: any;  
+};
+
+export let generate:(node:AST.Node, opts:EscodegenOptions)=>string = escodegen['generate']
